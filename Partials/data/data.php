0c86d70d2f1eb74ea54b355e405067631d4e5c77
Luigi <luigismilzo@live.com>
2020-05-20 17:52:30 +0200

    Php echos

--- Partials/data/data.php
+++ Partials/data/data.php
@@ -6,58 +6,58 @@ $albumData = [
         'year' => '1980',
         'poster_path' => "dist/img/iron-maiden.jpg" 
     ],
-    // [
-    //     'artist' => 'Iron Maiden',
-    //     'album_title' => 'Killers',
-    //     'year' => '1981',
-    //     'poster_path' => ...
-    // ],
-    // [
-    //     'artist' => 'Iron Maiden',
-    //     'album_title' => 'The number of the beast',
-    //     'year' => '1982',
-    //     'poster_path' => ...
-    // ],
-    // [
-    //     'artist' => 'Iron Maiden',
-    //     'album_title' => 'Piece of mind',
-    //     'year' => '1983',
-    //     'poster_path' => ...
-    // ],
-    // [
-    //     'artist' => 'Iron Maiden',
-    //     'album_title' => 'Powerslave',
-    //     'year' => '1984',
-    //     'poster_path' => ...
-    // ],
-    // [
-    //     'artist' => 'Iron Maiden',
-    //     'album_title' => 'Somewhere in time',
-    //     'year' => '1986',
-    //     'poster_path' => ...
-    // ],
-    // [
-    //     'artist' => 'Iron Maiden',
-    //     'album_title' => 'Seventh son of a seventh son',
-    //     'year' => '1988',
-    //     'poster_path' => ...
-    // ],
-    // [
-    //     'artist' => 'Iron Maiden',
-    //     'album_title' => 'No prayer for the dying',
-    //     'year' => '1990',
-    //     'poster_path' => ...
-    // ],
-    // [
-    //     'artist' => 'Iron Maiden',
-    //     'album_title' => 'Fear of the dark',
-    //     'year' => '1992',
-    //     'poster_path' => ...
-    // ],
-    // [
-    //     'artist' => 'Iron Maiden',
-    //     'album_title' => 'The X factor',
-    //     'year' => '1995',
-    //     'poster_path' => ...
-    // ],
+    [
+        'artist' => 'Iron Maiden',
+        'album_title' => 'Killers',
+        'year' => '1981',
+        'poster_path' => "dist/img/killers.jpg"
+    ],
+    [
+        'artist' => 'Iron Maiden',
+        'album_title' => 'The number of the beast',
+        'year' => '1982',
+        'poster_path' => "dist/img/number.jpg"
+    ],
+    [
+        'artist' => 'Iron Maiden',
+        'album_title' => 'Piece of mind',
+        'year' => '1983',
+        'poster_path' => "dist/img/piece-of-mind.jpg"
+    ],
+    [
+        'artist' => 'Iron Maiden',
+        'album_title' => 'Powerslave',
+        'year' => '1984',
+        'poster_path' => "dist/img/powerslave.jpg"
+    ],
+    [
+        'artist' => 'Iron Maiden',
+        'album_title' => 'Somewhere in time',
+        'year' => '1986',
+        'poster_path' => "dist/img/somewhere-in-time.jpg"
+    ],
+    [
+        'artist' => 'Iron Maiden',
+        'album_title' => 'Seventh son of a seventh son',
+        'year' => '1988',
+        'poster_path' => "dist/img/seventh-son.jpg"
+    ],
+    [
+        'artist' => 'Iron Maiden',
+        'album_title' => 'No prayer for the dying',
+        'year' => '1990',
+        'poster_path' => "dist/img/no-prayer.jpg"
+    ],
+    [
+        'artist' => 'Iron Maiden',
+        'album_title' => 'Fear of the dark',
+        'year' => '1992',
+        'poster_path' => "dist/img/fear.jpg"
+    ],
+    [
+        'artist' => 'Iron Maiden',
+        'album_title' => 'The X factor',
+        'year' => '1995',
+        'poster_path' => "dist/img/the-x-factor.jpg"
+    ],
 ];
--- index.php
+++ index.php
@@ -21,7 +21,10 @@ include __DIR__ . "/Partials/data/data.php"
         <div class="Album u-flex u-container">
             <?php foreach ($albumData as $data) : ?>
             <div class="Album-card">
-                <img src="<?php echo $data['poster_path']; ?>" alt="">
+                <img src="<?php echo $data['poster_path']; ?>" alt="cover">
+                <h4><?php echo $data['album_title'] ?></h4>
+                <h5><?php echo $data['artist'] ?></h5>
+                <h6><?php echo $data['year'] ?></h6>
             </div>
             <?php endforeach ?>
         </div>

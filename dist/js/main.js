08f7300df8f64c5e56fc4345e542b38fbe357ab2
Luigi <luigismilzo@live.com>
2020-05-21 13:06:21 +0200

    Filter functionality (case sensitive)

--- dist/js/main.js
+++ dist/js/main.js
@@ -95,11 +95,23 @@
 
 $(document).ready(function () {
   // References
-  var container = $('.Album'); // Handlebars init
+  var container = $('.Album');
+  var searchInput = $('#SearchInput');
+  var searchBtn = $('#SearchButton'); // Handlebars init
 
   var source = $('#AlbumTemplate').html();
-  var template = Handlebars.compile(source);
-  albumLoad(container, template);
+  var template = Handlebars.compile(source); // Showing albums
+
+  albumLoad(container, template); // Search
+
+  searchBtn.click(function () {
+    albumSearch(container, template, searchInput);
+  });
+  searchInput.keypress(function (e) {
+    if (e.which == 13) {
+      albumSearch(container, template, searchInput);
+    }
+  });
 }); // <- End ready
 
 /*************
@@ -121,6 +133,41 @@ function albumLoad(container, template) {
         };
         var html = template(context);
         container.append(html);
+      }
+    },
+    error: function error() {
+      console.log('Error');
+    }
+  });
+}
+
+function albumSearch(container, template, searchInput) {
+  var searchVal = searchInput.val().trim();
+  $.ajax({
+    url: 'http://localhost/php-ajax-dischi/partials/script/encode.php',
+    method: 'GET',
+    success: function success(res) {
+      container.html('');
+
+      if (searchVal != '') {
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].album_title == searchVal) {
+            var album = res[i];
+            var context = {
+              posterPath: album.poster_path,
+              title: album.album_title,
+              artist: album.artist,
+              year: album.year
+            };
+            var html = template(context);
+            container.append(html);
+          }
+        }
+
+        searchInput.val('').blur();
+      } else {
+        searchInput.focus();
+        container.append('Campo di ricerca vuoto');
       }
     },
     error: function error() {
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,30 @@
 $(document).ready(function () {
     // References
     var container = $('.Album');
+    var searchInput = $('#SearchInput');
+    var searchBtn = $('#SearchButton');
+    
 
 
     // Handlebars init
     var source = $('#AlbumTemplate').html();
     var template = Handlebars.compile(source);
 
+    // Showing albums
     albumLoad(container, template);
 
+    // Search
+    searchBtn.click(function() {
+        albumSearch (container, template, searchInput);
+    });
 
-    
+    searchInput.keypress(function(e) {
+        if (e.which == 13) {
+            albumSearch (container, template, searchInput);
+        }
+    });
+
+
 }); // <- End ready
 
 /*************
@@ -35,6 +49,49 @@ function albumLoad(container, template) {
                 var html = template(context);
                 container.append(html);
             }
+        },
+        error: function() {
+            console.log('Error');
+        }
+    });
+}
+
+function albumSearch (container, template, searchInput) {
+    var searchVal = searchInput.val().trim();
+
+    $.ajax({
+        url: 'http://localhost/php-ajax-dischi/partials/script/encode.php',
+        method: 'GET',
+        success: function(res) {
+            container.html('');
+            
+            if (searchVal != '') {
+                
+                for (var i = 0; i < res.length; i++) {
+                    
+                    
+                    if ( res[i].album_title == searchVal ) {
+                        var album = res[i];
+                        
+                        var context = {
+                            posterPath: album.poster_path,
+                            title: album.album_title,
+                            artist: album.artist,
+                            year: album.year
+                        }
+                        
+                        var html = template(context);
+                        container.append(html);
+                    }
+    
+                }
+                
+                searchInput.val('').blur();
+            } else {
+                searchInput.focus()
+                container.append('Campo di ricerca vuoto')
+            }
+            
         },
         error: function() {
             console.log('Error');
